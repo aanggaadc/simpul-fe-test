57f043a87c958c7b4e56975972f04e08c4a41c9f
anggara.setiawan <aanggaadc@gmail.com>
2024-08-18 20:34:06 +0700

    create task provider

---
+++ src/providers/task-provider.tsx
@@ -0,0 +1,35 @@
+import { useContext, createContext, useState } from "react";
+import { tasks } from "@/fixtures/task";
+
+type TaskType = (typeof tasks)[0];
+
+interface TaskContextType {
+  taskList: TaskType[];
+  setTaskList: React.Dispatch<React.SetStateAction<TaskType[]>>;
+}
+
+interface TaskProviderProps {
+  children: React.ReactNode;
+}
+
+const ChatContext = createContext<TaskContextType | undefined>(undefined);
+
+export const useTaskState = () => {
+  const context = useContext(ChatContext);
+  if (context === undefined) {
+    throw new Error("useTaskState must be used within a TaskContextProvider");
+  }
+  return context;
+};
+
+export const TaskProvider: React.FC<TaskProviderProps> = ({ children }) => {
+  const [taskList, setTaskList] = useState(tasks);
+
+  return (
+    <ChatContext.Provider value={{ taskList, setTaskList }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export default TaskProvider;
